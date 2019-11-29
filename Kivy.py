1ab28b7696ab224047a5f6ef7bb60a1de8cb2c0b
joeli <joelimgu@gmail.com>
2019-11-29 19:38:48 +0100

    not enterely working

--- Kivy.py
+++ Kivy.py
@@ -2,12 +2,25 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import tkinter as tk
+from kivy import Config
+
+#Gets the screen size using tkinter
+root = tk.Tk()
+root.withdraw()
+
+print(root.winfo_screenwidth())
+print(root.winfo_screenheight())
+
+#sets the screen into a 16/9 ratio in a vertical position taking the screen size of the device
+Config.set('graphics', 'width', int(root.winfo_screenheight()*9/16))
+Config.set('graphics', 'height', root.winfo_screenheight() - 60)
+Config.write()
 
 Builder.load_file("pongA.kv")
 
 
 class RoundedButton(Button):
-
     def __init__(self, *kwargs):
         super.__init__(*kwargs)
 
